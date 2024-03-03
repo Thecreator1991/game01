e4a565e39d812ac0ce1135fb680fa6c779ae8f3f
Thecreator1991 <140057160+Thecreator1991@users.noreply.github.com>
2024-03-03 22:30:13 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include<SDL.h>
+#include<SDL_image.h>
+#include<iostream>
+
+using namespace std;
+
+int main(int argv, char* argc[]){
+
+    SDL_Init(SDL_INIT_VIDEO);
+    IMG_Init(IMG_INIT_PNG);
+
+    SDL_Window* window = SDL_CreateWindow("Game",300,200,800,400,0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+
+    SDL_Surface* surface = SDL_LoadBMP("texture/pic.bmp");
+    SDL_Surface* chr = IMG_Load("texture/chr.png");
+    SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surface);
+    SDL_Texture* entity = SDL_CreateTextureFromSurface(renderer, chr);
+
+
+    bool running = true;
+
+    while(running){
+        SDL_Event e;
+        while(SDL_PollEvent(&e)){
+            if(e.type == SDL_QUIT)running = false;
+
+        }
+
+    }
+
+    SDL_DestroyTexture(tex);
+    SDL_DestroyTexture(entity);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+return 0;
+}
